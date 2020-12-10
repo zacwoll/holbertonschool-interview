ba4348ec2f1ebc03e6a9630225ec4412c4312fc1
zacwoll <zacwoll@gmail.com>
2020-12-09 19:15:13 -0800

    Switched the if gate so that the latter can depend on the first being true

--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -1,6 +1,6 @@
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-boxes = [[1], [2], [3], [4], []]
+boxes = [[1], [2], [3], [4, 9], []]
 print(canUnlockAll(boxes))
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
