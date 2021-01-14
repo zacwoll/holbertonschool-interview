a2c4703cae6542a817ff6217f96f09d24769e55c
zacwoll <zacwoll@gmail.com>
2021-01-14 15:53:52 -0800

    Removed unused function

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -4,36 +4,6 @@
 #include "sandpiles.h"
 
 /**
- * print_grid_sum prints the sum of two 3x3 grids
- * @grid1: the first grid
- * @grid2: the second grid
- */
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid1[i][j]);
-        }
-
-        printf(" %c ", (i == 1 ? '+' : ' '));
-
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid2[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-/**
  * print_grid prints a 3x3 grid
  * @grid1: the first grid
  */
