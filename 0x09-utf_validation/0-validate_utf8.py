5d8ee42a48256b2677fbdbd7e786afd9f8b051c4
zacwoll <zacwoll@gmail.com>
2021-02-16 18:58:53 -0800

    0x09

---
+++ 0x09-utf_validation/0-validate_utf8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """ Validate UTF-8 Encodings """
+
+    def get_byte_count(num):
+        """ Get byte count of character """
+        prefix_dict = {
+            "0": 1,
+            "110": 2,
+            "1110": 3,
+            "11110": 4,
+            }
+
+        for prefix in prefix_dict:
+            if num.startswith(prefix):
+                return prefix_dict[prefix]
+
+        return False
+
+    num_bytes = 0
+    for num in data:
+        num = format(num, '#010b')[-8:]
+
+        # start of new char
+        if num_bytes == 0:
+            num_bytes = get_byte_count(num)-1
+            if num_bytes is None:
+                return False
+
+        # continue processing char
+        else:
+            if not num.startswith("10"):
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
