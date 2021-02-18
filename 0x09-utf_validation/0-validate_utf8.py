73396a1d4f70add3a5ed053584d161190d74eed1
zacwoll <zacwoll@gmail.com>
2021-02-18 11:08:29 -0800

    Holberton Checker doesn't like right answers.

--- 0x09-utf_validation/0-validate_utf8.py
+++ 0x09-utf_validation/0-validate_utf8.py
@@ -6,22 +6,6 @@
 
 def validUTF8(data):
     """ Validate UTF-8 Encodings """
-
-    def get_byte_count(num):
-        """ Get byte count of character """
-        prefix_dict = {
-            "0": 1,
-            "110": 2,
-            "1110": 3,
-            "11110": 4,
-            }
-
-        for prefix in prefix_dict:
-            if num.startswith(prefix):
-                return prefix_dict[prefix]
-
-        return False
-
     num_bytes = 0
     for num in data:
         num = format(num, '#010b')[-8:]
@@ -39,3 +23,19 @@ def validUTF8(data):
             num_bytes -= 1
 
     return num_bytes == 0
+
+
+def get_byte_count(num):
+    """ Get byte count of character """
+    prefix_dict = {
+        "0": 1,
+        "110": 2,
+        "1110": 3,
+        "11110": 4,
+        }
+
+    for prefix in prefix_dict:
+        if num.startswith(prefix):
+            return prefix_dict[prefix]
+
+    return False
