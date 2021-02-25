76a607e2d0f31025365dc5549b1b6b6f1bb16768
zacwoll <zacwoll@gmail.com>
2021-02-25 21:32:57 +0000

    Went to vagrant shell and ran it through betty style

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -3,50 +3,69 @@
 #include "slide_line.h"
 
 
-void rev_list(int *line, size_t size) {
-    int temp = 0;
-    for (int i = 0, j = size - 1; i < j; i++) {
-        temp = line[i];
-        line[i] = line[j];
-        line[j] = temp;
-    }
+/**
+ * rev_list - reverses list for right side calculation
+ * @line: line to be reversed
+ * @size: size of the list
+ */
+void rev_list(int *line, size_t size)
+{
+	int temp = 0;
+
+	for (int i = 0, j = size - 1; i < j; i++)
+	{
+		temp = line[i];
+		line[i] = line[j];
+		line[j] = temp;
+	}
 }
 
+/**
+ * slide_line - function to slide values to the left inside line
+ * @line: array of values
+ * @size: size of line
+ * @direction: 1 or -1, for L and R respectively
+ * Return: 1 or 0 if function succeeded/failed
+ */
 int slide_line(int *line, size_t size, int direction)
 {
-    bool merged_last = false;
-    int val;
-    int start = 0, end = size - 1;
+	bool merged_last = false;
+	int val;
+	int start = 0, end = size - 1;
 
-    if (!line)
-        return (0);
-    if (direction == -1) {
-        rev_list(line, size);
-    }
-    for (int i = start; i <= end; i++) {
-        if (line[i] == 0)
-            continue;
-        val = line[i];
-        for (int j = i - 1; j >= start && j <= end; j -= 1) {
-            if (line[j] == val && merged_last == false) {
-                line[i] = 0;
-                line[j] += val;
-                val = line[j];
-                merged_last = true;
-            }
-            else if (line[j] > 0) {
-                line[j + 1] = val;
-                line[i] = 0;
-                merged_last = false;
-                start++;
-                break;
-            }
-            else {
-                line[j] = 0;
-            }
-        }
-    }
-    if (direction == -1)
-        rev_list(line, size);
-    return (1);
+	if (!line)
+		return (0);
+	if (direction == -1)
+	{
+		rev_list(line, size);
+	}
+	for (int i = start; i <= end; i++)
+	{
+		if (line[i] == 0)
+			continue;
+		val = line[i];
+		for (int j = i - 1; j >= start && j <= end; j -= 1)
+		{
+			if (line[j] == val && merged_last == false)
+			{
+				line[i] = 0;
+				line[j] += val;
+				val = line[j];
+				merged_last = true;
+			} else if (!(line[j] > 0))
+			{
+				line[j] = 0;
+			} else
+			{
+				line[j + 1] = val;
+				line[i] = 0;
+				merged_last = false;
+				start++;
+				break;
+			}
+		}
+	}
+	if (direction == -1)
+		rev_list(line, size);
+	return (1);
 }
