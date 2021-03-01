c9516457555521025cbc37d29cc099c08be6231f
zacwoll <zacwoll@gmail.com>
2021-03-01 12:22:13 -0800

    Style changes and comments added

--- 0x0B-menger/0-main.c
+++ 0x0B-menger/0-main.c
@@ -13,16 +13,16 @@
  */
 int main(int ac, char **av)
 {
-    int level;
+	int level;
 
-    if (ac < 2)
-    {
-        fprintf(stderr, "Usage: %s level\n", av[0]);
-        return (EXIT_FAILURE);
-    }
+	if (ac < 2)
+	{
+		fprintf(stderr, "Usage: %s level\n", av[0]);
+		return (EXIT_FAILURE);
+	}
 
-    level = atoi(av[1]);
-    menger(level);
+	level = atoi(av[1]);
+	menger(level);
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
