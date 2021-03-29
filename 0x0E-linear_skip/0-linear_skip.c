74a69b9229f478d79c28ddc0f92389aa56a55617
zacwoll <zacwoll@gmail.com>
2021-03-29 12:12:12 -0700

    Added solution files

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,59 @@
+#include "search.h"
+
+size_t get_end_index(skiplist_t *list)
+{
+    size_t end_index;
+    skiplist_t *former;
+    while (list)
+    {
+        former = list;
+        list = list->express;
+    }
+    list = former;
+    while (list)
+    {
+        former = list;
+        list = list->next;
+    }
+    return (former->index);
+}
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *lower = list;
+    size_t end_index = get_end_index(list);
+    // if given skiplist has error
+    if (!list)
+        return (NULL);
+    else
+        list = list->express;
+
+    while (list && list->n < value)
+    {
+        printf("Value checked at index [%llu] = [%llu]\n", list->index, list->n);
+        lower = list;
+        list = list->express;
+    }
+    if (!list || list->n > value)
+    {
+        if (list)
+            end_index = list->index;
+        printf("Value found between indexes [%llu] and [%llu]\n",
+            lower->index, end_index);
+        printf("Value checked at index [%llu] = [%llu]\n", lower->index, lower->n);
+        list = lower;
+    }
+    else if (list->n == value)
+        return (list);
+
+    while (list && list->n < value)
+    {
+        lower = list;
+        list = list->next;
+        printf("Value checked at index [%llu] = [%llu]\n", list->index, list->n);
+    }
+    if (!list || list->n != value)
+        return (NULL);
+    else if (list->n == value)
+        return (list);
+}
---
+++ 0x0E-linear_skip/search.h
@@ -0,0 +1,29 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct skiplist_t
+{
+	int n;
+	size_t index;
+	struct skiplist_t *express;
+	struct skiplist_t *next;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+
+#endif /* SEARCH_H */
