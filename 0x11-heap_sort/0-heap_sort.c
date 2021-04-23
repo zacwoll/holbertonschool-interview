5e08d43d3c623c23296847efb6a092c288b44ce6
zacwoll <zacwoll@gmail.com>
2021-04-22 20:32:30 -0700

    updated heap_sort

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,62 +1,77 @@
 #include "sort.h"
 
-/**
- * swap - swaps 2 elements in an int array
- * @array: int array
- * @a: the first element index
- * @b: the second element index
- */
-void swap(int *array, int a, int b)
-{
-	if (a == b)
-		return;
-	array[a] ^= array[b];
-	array[b] ^= array[a];
-	array[a] ^= array[b];
-}
 
 /**
- * heapify - converts array to max heap
- * @array: int array
- * @heap_size: size of the array to be heaped
- * @i: recursive element to set as largest value
- * @size: size of array
+ * heap_sort - implement heap sort
+ *@array: data of integers to sort
+ *@size: size of array
  */
-void heapify(int *array, size_t heap_size, size_t i, size_t size)
-{
-	size_t largest = i;
-	size_t l = 2 * i + 1, r = 2 * i + 2;
 
-	if (l < heap_size && array[l] > array[largest])
-		largest = l;
-	if (r < heap_size && array[r] > array[largest])
-		largest = r;
-
-	if (largest != i)
-	{
-		swap(array, i, largest);
-		print_array(array, size);
-		heapify(array, heap_size, largest, size);
-	}
-}
-
-/**
- * heap_sort - sorts array using heap_sort
- * @array: int array to be sorted
- * @size: size of array
- */
 void heap_sort(int *array, size_t size)
 {
-	size_t i;
+	int end = size - 1;
 
-    /* I can refactor this with 99% certainty */
-	for (i = size / 2 - 1; i > 0; i--)
-		heapify(array, size, i, size);
-	heapify(array, size, 0, size);
-	for (i = size - 1; i > 0; i--)
+	if (size < 2 || array == NULL)
+		return;
+
+	heapify(array, size);
+
+	while (end > 0)
 	{
-		swap(array, 0, i);
-		print_array(array, size);
-		heapify(array, i, 0, size);
+		swap(array, size, 0, end);
+		siftDown(array, size, 0, --end);
 	}
+}
+
+/**
+ * heapify - put elements of array in heap order
+ *@array: data to modify
+ *@size: size of array
+ */
+void heapify(int *array, size_t size)
+{
+	int start = (size / 2) - 1;
+
+	while (--start >= 0)
+		siftDown(array, size, start, size - 1);
+}
+
+/**
+ * siftDown - Repairs heap
+ *@array: data to modify
+ *@size: size of array
+ *@start: start index of array
+ *@end: end index of array
+ */
+void siftDown(int *array, size_t size, int start, int end)
+{
+	int left = start * 2 + 1;
+	int right = left + 1;
+	int max = start;
+
+	if (left <= end && array[left] > array[max])
+		max = left;
+	if (right <= end && array[right] > array[max])
+		max = right;
+	if (max == start)
+		return;
+
+	swap(array, size, start, max);
+	siftDown(array, size, max, end);
+}
+
+/**
+ * swap - swap values in array at indecies A and B
+ *@array: data to modify
+ *@size: size of array
+ *@indexA: start index of array
+ *@indexB: end index of array
+ */
+void swap(int *array, size_t size, int indexA, int indexB)
+{
+	int temp = array[indexA];
+
+	array[indexA] = array[indexB];
+	array[indexB] = temp;
+	print_array(array, size);
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,9 +1,13 @@
-#ifndef SORT_H
-#define SORT_H
+
+#ifndef _SORT_H_
+#define _SORT_H_
 
 #include <stdlib.h>
 
+void print_array(const int *, size_t);
 void heap_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
+void heapify(int *array, size_t size);
+void siftDown(int *array, size_t size, int start, int end);
+void swap(int *array, size_t size, int indexA, int indexB);
 
-#endif /* PALINDROME_H */
+#endif /* _SORT_H_ */
